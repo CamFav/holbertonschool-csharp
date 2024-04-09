9a2d4ca7e70eaa65148297d4444ae26bc5fae4ff
CamilleFavriel <camillefvrl@gmail.com>
2024-04-09 19:40:29 +0200

    Task 4 - Print integer stored in variable

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -1,10 +1,10 @@
-using System;
+﻿using System;
 
 class Program
 {
 	static void Main(string[] args)
 	{
 		int number = 972;
-		Console.WriteLine("{0} Mission Street", number);
+		Console.WriteLine($"{number} Mission Street");
 	}
 }
