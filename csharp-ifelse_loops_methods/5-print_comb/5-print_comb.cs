cb9de34ea583942305ddca909ad3ed5235df352c
CamilleFavriel <camillefvrl@gmail.com>
2024-04-12 11:32:46 +0200

    Task 5 - Prints number from 0 to 99 with separator

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -1,1 +1,24 @@
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            // Print the number with two digits
+            Console.Write($"{i:D2}");
+
+            // If it's not the last number, print ", "
+            if (i != 99)
+            {
+                Console.Write(", ");
+            }
+            // If it's the last number, print a new line
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
