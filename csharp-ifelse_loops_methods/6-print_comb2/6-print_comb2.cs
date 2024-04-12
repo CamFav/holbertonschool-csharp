547ab10152c7f748d055bf4a709f1aaec0726bb2
CamilleFavriel <camillefvrl@gmail.com>
2024-04-12 11:46:50 +0200

    [fix] updated loops

--- csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -1,1 +1,17 @@
+using System;
 
+class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 0; i < 8; i++) {
+                for (j = 1; j <= 9; j++) {
+                    if (i < j) {
+                        Console.Write("{0}{1}, ", i, j);
+                    }
+                }
+            }
+            Console.WriteLine("89");
+        }
+    }
