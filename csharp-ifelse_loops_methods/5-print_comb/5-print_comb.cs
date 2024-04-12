4534b16a718993b3ba4d79903d84b28240a1f6b9
CamilleFavriel <camillefvrl@gmail.com>
2024-04-12 11:37:10 +0200

    Requirement fail - Deleted one Console.Write

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -7,18 +7,11 @@ class Program
         for (int i = 0; i < 100; i++)
         {
             // Print the number with two digits
-            Console.Write($"{i:D2}");
-
-            // If it's not the last number, print ", "
-            if (i != 99)
-            {
-                Console.Write(", ");
-            }
-            // If it's the last number, print a new line
+            if(i != 99)
+                Console.Write("{0:D2}, ", i);
             else
-            {
-                Console.WriteLine();
-            }
+            // If it's the last number, print a new line
+                Console.Write("{0:D2}\n", i);
         }
     }
 }
