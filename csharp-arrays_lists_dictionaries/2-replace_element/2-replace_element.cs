989210c6a6c340fa55a8e1b60ec155b92d0da246
CamilleFavriel <camillefvrl@gmail.com>
2024-04-16 15:41:38 +0200

    [T2] Replace element in array

--- csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -1,1 +1,21 @@
+using System;
 
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        // Get the length of the array
+        int length = array.GetLength(0);
+
+        if (index < 0 || index >= length)
+        {
+            Console.WriteLine("Index out of range");
+            return array;
+        }
+        else
+        {
+            array[index] = n;
+            return array;
+        }
+    }
+}
