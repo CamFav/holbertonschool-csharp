c6c3bd9410d67a89d9b8598612b3bc4716a0b3f3
CamilleFavriel <camillefvrl@gmail.com>
2024-04-16 15:26:28 +0200

    [T1] Access an element in an array

--- csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -1,1 +1,20 @@
+using System;
 
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        // Get the length of the array
+        int length = array.GetLength(0);
+
+        if (index < 0 || index >= length)
+        {
+            Console.WriteLine("Index out of range");
+            return -1;
+        }
+        else
+        {
+            return array[index];
+        }
+    }
+}
