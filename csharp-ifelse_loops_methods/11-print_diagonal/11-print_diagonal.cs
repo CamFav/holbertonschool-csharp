07e744f44ddfdde0d50e6b8eac3648191d293210
CamilleFavriel <camillefvrl@gmail.com>
2024-04-16 14:32:45 +0200

    [T11] Draws a diagonal line

--- csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -1,1 +1,24 @@
+using System;
 
+public class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+
+        for (int i = 0; i < length; i++)
+        {
+            // Print spaces before the \
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("\\");
+        }
+        Console.WriteLine();
+    }
+}
