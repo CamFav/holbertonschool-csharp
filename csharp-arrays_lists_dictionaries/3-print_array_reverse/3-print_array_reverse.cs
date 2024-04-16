d8f547169e4ba4adde7b00460f9dd2aca4e0ccae
CamilleFavriel <camillefvrl@gmail.com>
2024-04-16 15:55:01 +0200

    [T3] Print an array in reverse

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -1,1 +1,18 @@
+using System;
 
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null)
+        {
+            return;
+        }
+
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
