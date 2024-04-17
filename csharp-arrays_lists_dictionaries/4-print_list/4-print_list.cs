80bc0fda75c7db8e5bcdda4d9342fec33da0757d
CamilleFavriel <camillefvrl@gmail.com>
2024-04-17 17:31:34 +0200

    [T4] Create a new list and print a list of integers

--- csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -1,1 +1,26 @@
+using System;
 
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> newList = new List<int>();
+
+        int i = 0;
+        while (i < size)
+        {
+            newList.Add(i);
+            Console.Write(i + " ");
+            i++;
+        }
+        Console.WriteLine();
+
+        return newList;
+    }
+}
