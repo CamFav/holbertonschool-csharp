36f50e101ed7e489227ba95e4bd44d803c54d286
CamilleFavriel <camillefvrl@gmail.com>
2024-04-17 18:34:22 +0200

    [T7] Deletes a element at a specific position in a list

--- csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -1,1 +1,27 @@
+using System;
 
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        // Check if index is out of range
+        if (index < 0 || index >= myList.Count)
+        {
+            Console.WriteLine("Index is out of range");
+            return myList;
+        }
+
+        List<int> newList = new List<int>();
+
+        // Copy elements, excluding the element at the specified index
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (i != index)
+            {
+               newList.Add(myList[i]);
+            }
+        }
+
+        return newList;
+    }
+}
