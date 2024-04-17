6bc203f6bae3e94abfed4cbca82095ec25e7a11e
CamilleFavriel <camillefvrl@gmail.com>
2024-04-17 18:06:18 +0200

    [T5] Find the biggest intneger of a list without using Max()

--- csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -1,1 +1,26 @@
+using System;
 
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+
+        int maxInt = myList[0];
+
+        foreach (int num in myList)
+        {
+            if (num > maxInt)
+            {
+                maxInt = num;
+            }
+        }
+
+        // Retourne le plus grand entier trouvé
+        return maxInt;
+    }
+}
