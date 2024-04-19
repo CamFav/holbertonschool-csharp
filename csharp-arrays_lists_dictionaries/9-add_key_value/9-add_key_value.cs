0141df3a3c380f05daa33a33c2e93f26780404aa
CamilleFavriel <camillefvrl@gmail.com>
2024-04-19 11:19:46 +0200

    [T9] Method that adds a key and value to a dictionary

--- csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
+++ csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -1,1 +1,21 @@
+using System;
+using System.Collections.Generic;
 
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        // If the key already exists, update its value
+        if (myDict.ContainsKey(key))
+        {
+            myDict[key] = value;
+        }
+        // If the key doesn't exist, add it with the given value
+        else
+        {
+            myDict.Add(key, value);
+        }
+
+        return myDict;
+    }
+}
