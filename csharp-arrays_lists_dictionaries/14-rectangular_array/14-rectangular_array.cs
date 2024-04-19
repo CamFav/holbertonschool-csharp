c6c1610494d541cd14cb91073f0ca221540667ac
CamilleFavriel <camillefvrl@gmail.com>
2024-04-19 14:30:00 +0200

    [T14] Creates and prints a 5 by two-dimensional array

--- csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -1,1 +1,22 @@
+using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a 5 by 5 two-dimensional array
+        int[,] array = new int[5, 5];
+
+        // Initialize index [2,2] to 1 and all other indices to 0
+        array[2, 2] = 1;
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
