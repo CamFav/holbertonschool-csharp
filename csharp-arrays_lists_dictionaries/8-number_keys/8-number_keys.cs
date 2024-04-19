ad659a94236a926c4635369c0383fc64bcb1a236
CamilleFavriel <camillefvrl@gmail.com>
2024-04-19 11:09:22 +0200

    [Fix] Using KeyValuePair to access value and key

--- csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -1,14 +1,17 @@
 using System;
+using System.Collections.Generic;
 
 class Dictionary
 {
     public static int NumberOfKeys(Dictionary<string, string> myDict)
     {
         int count = 0;
-        foreach (var key in myDict.Keys)
+
+        foreach (KeyValuePair<string, string> pair in myDict)
         {
             count++;
         }
+
         return count;
     }
 }
