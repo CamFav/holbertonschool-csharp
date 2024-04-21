9fbf6bd09202a247ed59a55e509550e5be593b9b
CamilleFavriel <camillefvrl@gmail.com>
2024-04-21 14:00:02 +0200

    [T1]Returns a sorted list of common elements in two lists without using linq or SortedList

--- csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -1,1 +1,17 @@
+using System;
+using System.Collections.Generic;
 
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        // Find common elements
+        HashSet<int> set = new HashSet<int>(list1);
+        set.IntersectWith(list2);
+
+        List<int> commonElements = new List<int>(set);
+        commonElements.Sort();
+
+        return commonElements;
+    }
+}
