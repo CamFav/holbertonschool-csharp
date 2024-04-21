58c9d224fd3cdaa6178de69a0c291b17cc660f65
CamilleFavriel <camillefvrl@gmail.com>
2024-04-21 17:19:31 +0200

    [Fix] Deleted useless variables

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -8,10 +8,10 @@ using System.Collections.Generic;
 
 class MyStack
 {
-    // Main - entry point
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
         Console.WriteLine("Number of items: {0}", aStack.Count);
+
         if (aStack.Count == 0)
         {
             Console.WriteLine("Stack is empty");
@@ -20,12 +20,16 @@ class MyStack
         {
             Console.WriteLine("Top item: {0}", aStack.Peek());
         }
+
         Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search));
+
+        // Remove all items up to and including search.
         while (aStack.Contains(search))
-        {
             aStack.Pop();
-        }
+
         aStack.Push(newItem);
+
         return aStack;
+
     }
 }
