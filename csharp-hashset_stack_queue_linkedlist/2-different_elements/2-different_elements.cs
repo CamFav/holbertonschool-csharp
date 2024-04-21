b94780a6029f01391cb4115a0e77098fda31cce9
CamilleFavriel <camillefvrl@gmail.com>
2024-04-21 14:51:47 +0200

    [T2] Returns a sorted list of all elemnents present in one or the other list but not both using hashset

--- csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -1,1 +1,22 @@
+// method that returns a sorted list of all elements present in one or the other list but not both.
+using System;
+using System.Collections.Generic;
 
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> set1 = new HashSet<int>(list1);
+        HashSet<int> set2 = new HashSet<int>(list2);
+
+        set1.ExceptWith(set2);
+        set2.ExceptWith(list1);
+
+        set1.UnionWith(set2);
+
+        List<int> differentElements = new List<int>(set1);
+        differentElements.Sort();
+
+        return differentElements;
+    }
+}
