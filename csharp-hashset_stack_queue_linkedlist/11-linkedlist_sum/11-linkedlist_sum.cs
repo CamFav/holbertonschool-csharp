fa3e9a0a451a0decb55b40ce00f8518e390e28ff
CamilleFavriel <camillefvrl@gmail.com>
2024-04-22 14:04:09 +0200

    [T11] Returns the sum of all the data of a LinkedList

--- csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -1,1 +1,17 @@
+// Method that returns the sum of all the data of a LinkedList
+using System;
+using System.Collections.Generic;
 
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int sum = 0;
+
+        foreach (int num in myLList)
+        {
+            sum += num;
+        }
+        return sum;
+    }
+}
