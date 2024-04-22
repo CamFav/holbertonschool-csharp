bf9f4af6a0a8d27606b2149edde6518efd91544f
CamilleFavriel <camillefvrl@gmail.com>
2024-04-22 11:54:48 +0200

    [T8] Finds a value in a LinkedList and returns its index position

--- csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -1,1 +1,21 @@
+// method that finds a value in a LinkedList and returns its index position in the list.
+using System;
+using System.Collections.Generic;
 
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int i = 0;
+
+        foreach (int item in myLList)
+        {
+            if (item == value)
+            {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+}
