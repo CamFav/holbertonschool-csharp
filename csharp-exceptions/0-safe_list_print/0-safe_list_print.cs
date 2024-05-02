fe55c8740093efd8f22420ddf8e0ca9ea7421008
CamilleFavriel <camillefvrl@gmail.com>
2024-05-02 13:02:54 +0200

    [T0] Method that prints n elements of a list using try and catch

--- csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -1,1 +1,21 @@
+using System;
+using System.Collections.Generic;
 
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        try
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+            return n;
+        }
+        catch
+        {
+            return myList.Count;
+        }
+    }
+}
