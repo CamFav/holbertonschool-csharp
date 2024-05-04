813ba9de4795e53012cf5851ebc995c21202574a
CamilleFavriel <camillefvrl@gmail.com>
2024-05-04 17:01:23 +0200

    [T2] Add a constructor to struct Dog that takes parameters

--- csharp-structs_enums/2-dog/2-dog.cs
+++ csharp-structs_enums/2-dog/2-dog.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Based on 1-dog, add a constructor to struct Dog that takes parameters.
+using System;
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string dogName, float dogAge, string dogOwner, Rating dogRating)
+    {
+        name = dogName;
+        age = dogAge;
+        owner = dogOwner;
+        rating = dogRating;
+    }
+}
+
+enum Rating { Good, Great, Excellent };
