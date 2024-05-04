c63d26012955b228a3d793fd8562839c0547beb3
CamilleFavriel <camillefvrl@gmail.com>
2024-05-04 16:20:52 +0200

    [T1] Define a new struct Dog with defined members

--- csharp-structs_enums/1-dog/1-dog.cs
+++ csharp-structs_enums/1-dog/1-dog.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Based on 0-dog, define a new struct Dog with the following members:
+// - name, type: public string
+// - age, type: public float
+// - owner, type: public string
+// - rating, type public Rating
+using System;
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
+
+enum Rating { Good, Great, Excellent };
