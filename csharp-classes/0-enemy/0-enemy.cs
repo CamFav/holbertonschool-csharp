0c86de36a5de4d2d4331f21b9cee0315f1db3df9
CamilleFavriel <camillefvrl@gmail.com>
2024-05-11 15:25:23 +0200

    [T1 & T2] Public class Zombie that defines a zombie by health

---
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -0,0 +1,22 @@
+﻿namespace Enemies
+{
+    /// <summary>
+    /// Represents a zombie enemy.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Represents the health of the zombie.
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Constructor for the Zombie class.
+        /// Health set to 0.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
