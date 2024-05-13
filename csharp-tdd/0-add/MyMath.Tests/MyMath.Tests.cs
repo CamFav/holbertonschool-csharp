1a9090e0496bf2358315ba9adf635c14735ba09a
CamilleFavriel <camillefvrl@gmail.com>
2024-05-13 13:25:21 +0200

    [Test] Comeback to previous versin

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,23 +1,56 @@
 using NUnit.Framework;
+using MyMath;
 
 namespace MyMath.Tests
 {
-    ///<summary> represents the tests for Operations Class </summary>
     [TestFixture]
-    public class Operations_Tests
+    public class OperationsTests
     {
-        /// <sumary> test Add method from MyMath Class </sumary>
         [Test]
-        [TestCase(5, 10)]
-        [TestCase(-5, 8)]
-        [TestCase(0, 4)]
-        [TestCase(1000, 2)]
-        [TestCase(0, 0)]
-        [TestCase(-4, -10)]
-        public void Add_whenTwoNumbersPassed_ReturnsSumResult(int a, int b)
+        public void Add_WhenCalled_ReturnsSumOfTwoNumbers()
         {
+            // Arrange
+            int a = 3;
+            int b = 5;
+
+            // Act
             int result = Operations.Add(a, b);
-            Assert.AreEqual(a + b, result);
+
+            // Assert
+            Assert.AreEqual(8, result);
+        }
+
+        [Test]
+        public void negativeInt()
+        {
+            int result = Operations.Add(2, -12);
+
+            Assert.AreEqual(-10, result);
+        }
+        
+        [Test]
+        public void bothNegativeInt()
+        {
+            int result = Operations.Add(-10, -10);
+
+            Assert.AreEqual(-20, result);
+        }
+
+        [Test]
+        public void ZeroInt()
+        {
+            int result = Operations.Add(0, -30);
+
+            Assert.AreEqual(-30, result);
+        }
+
+         [Test]
+        
+        public void BothZeroInt()
+        {
+            int result = Operations.Add(0, 0);
+
+            Assert.AreEqual(0, result);
         }
     }
 }
