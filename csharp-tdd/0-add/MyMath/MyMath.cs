1a86e2717c3cb28f0ec9bcc5a56f1d3ea58205e5
CamilleFavriel <camillefvrl@gmail.com>
2024-05-13 13:02:58 +0200

    [Fix] Deleted useless comment

--- csharp-tdd/0-add/MyMath/MyMath.cs
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -10,9 +10,6 @@ namespace MyMath
         /// <summary>
         /// Returns the sum
         /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <returns></returns>
         public static int Add(int a, int b)
         {
             return a + b;
