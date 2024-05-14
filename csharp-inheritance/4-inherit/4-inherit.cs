7f1743e63e89cb1f92643ac28a91feb2bc6219a4
CamilleFavriel <camillefvrl@gmail.com>
2024-05-14 17:03:38 +0200

    [T4] Empty class Dog

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// [Base Class] Defining an Animal
+/// </summary>
+public class Animal
+{
+
+}
+
+/// <summary>
+/// [Derived Class of Animal] Defining a Dog
+/// </summary>
+public class Dog : Animal
+{
+
+}
