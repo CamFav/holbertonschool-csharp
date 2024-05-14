9e213ead5720e239e812f1415dc46e9820d0d7d4
CamilleFavriel <camillefvrl@gmail.com>
2024-05-14 14:40:46 +0200

    [T1] GetType() - Typeof - is

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Obj 
+{
+    /// <summary>
+    /// Check if the object is an instance
+    /// </summary>
+    /// <param name="obj">Value to check</param>
+    /// <returns>Returns True if the object is an instance; Otherwise, False.</returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj is Array || obj.GetType() == typeof(Array))
+            return true;
+
+        return false;
+    }
+}
