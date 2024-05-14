3ffb032344cee23a1efcfb1769257213584cd530
CamilleFavriel <camillefvrl@gmail.com>
2024-05-14 15:16:21 +0200

    [T2] Check if subclass

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Obj 
+{
+    /// <summary>
+    /// Check if the object is a subclass
+    /// </summary>
+    /// <param name="derivedType">Derived Class</param>
+    /// <param name="baseType">Base Class</param>
+    /// <returns>Returns True if the object is an subclass; Otherwise, False.</returns>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return baseType.IsAssignableFrom(derivedType) && derivedType != baseType;
+    }   
+}
