b093c80da3cec96400730477bbd14e11fad1a8ae
CamilleFavriel <camillefvrl@gmail.com>
2024-05-14 14:24:31 +0200

    [T0] Check if an int or not

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Obj 
+{
+    /// <summary>
+    /// Check if a object if an int or not.
+    /// </summary>
+    /// <param name="obj">Value to check</param>
+    /// <returns>Returns True if the object is an in int; Otherwise, False.</returns>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj is int) {
+            return true;
+        }
+
+        return false;
+    }
+}
