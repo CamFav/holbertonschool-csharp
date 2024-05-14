e43a1ce718f564238eb2b38fa1cb140bc3f31196
CamilleFavriel <camillefvrl@gmail.com>
2024-05-14 16:42:14 +0200

    [T3] Use of GetMembers() method to get properties and methods of an object

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+class Obj 
+{
+    /// <summary>
+    /// Prints the name of available properties and methods of an object
+    /// </summary>
+    /// <param name="myObj">Object to print</param>
+    public static void Print(object myObj)
+    {
+        Type objType = myObj.GetType();
+
+        Console.WriteLine($"{objType.Name} Properties and Methods:");
+
+        // Iterate throught both properties and methods
+        foreach (MemberInfo member in objType.GetMembers())
+        {
+            Console.WriteLine(member.Name);
+        } 
+    }
+}
