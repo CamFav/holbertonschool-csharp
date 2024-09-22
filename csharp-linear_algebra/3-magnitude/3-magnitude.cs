85817aecbcd4635caa88b42404a170c81792becd
CamilleFavriel <camillefvrl@gmail.com>
2024-09-22 22:41:51 +0200

    method that calculates and returns the length of a given vector.

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class VectorMath
+{
+    /// <summary>
+    /// Calcul the magnitude of a 2D or 3D Vector
+    /// </summary>
+    /// <param name="vector">Vector</param>
+    /// <returns>Returns the length of a given vector.</returns>
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length == 2)
+            return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)), 2);
+        else if (vector.Length == 3)
+            return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)), 2);
+        else
+            return -1;
+    }
+}
