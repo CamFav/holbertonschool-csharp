f917fd6ffe93cc8243aef1a98a30bddbd39415f8
CamilleFavriel <camillefvrl@gmail.com>
2024-11-28 16:26:41 +0100

    0. Queue

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Queue<T>
+{
+    /// <summary>
+    /// Returns the type of the generic parameter T.
+    /// </summary>
+    /// <returns>String representing the type of T.</returns>
+    public string CheckType()
+    {
+        return typeof(T).ToString(); // Gets the runtime type of T
+    }
+}
