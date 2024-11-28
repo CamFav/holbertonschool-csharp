049171e74f6e0b0676366d862063baf6264a000e
CamilleFavriel <camillefvrl@gmail.com>
2024-11-28 16:38:49 +0100

    Use of Type instead of String

--- csharp-generics/0-queue/obj/Debug/netcoreapp2.1/0-queue.AssemblyInfo.cs
+++ csharp-generics/0-queue/obj/Debug/netcoreapp2.1/0-queue.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("0-queue")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0126fa211ec269651eb0c171e7a80984175a20f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bc09b01a6404b25c43180530414b036ef78d417")]
 [assembly: System.Reflection.AssemblyProductAttribute("0-queue")]
 [assembly: System.Reflection.AssemblyTitleAttribute("0-queue")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -1,11 +1,13 @@
-﻿public class Queue<T>
+﻿using System;
+
+public class Queue<T>
 {
     /// <summary>
     /// Returns the type of the generic parameter T.
     /// </summary>
     /// <returns>String representing the type of T.</returns>
-    public string CheckType()
+    public Type CheckType()
     {
-        return typeof(T).ToString(); // Gets the runtime type of T
+        return typeof(T); // Gets the runtime type of T
     }
 }
