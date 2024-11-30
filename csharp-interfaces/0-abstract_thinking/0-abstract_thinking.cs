3dc555403a5ae75813de017bec521e14da16e0ba
CamilleFavriel <camillefvrl@gmail.com>
2024-11-30 14:04:25 +0100

    Task: Abstract Class with ToString() Implementation

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Abstract base class with a name property and ToString to override
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Public name property of type string
+    /// </summary>
+    public string name { get; set; }
+
+    // Retrieves the type of the derived class and formats the string
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
