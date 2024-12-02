3c1116d45fe577be54a9fc9582c1ccf092516d7e
CamilleFavriel <camillefvrl@gmail.com>
2024-12-02 12:26:43 +0100

    Task: User interface Implementation

---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+/// <summary>
+/// Abstract base class with a name property and ToString to override.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Public name property of type string.
+    /// </summary>
+    public string name { get; set; }
+
+    /// <summary>
+    /// Retrieves the type of the derived class and
+    /// formats the string.
+    /// </summary>
+    /// <returns>A string in the format: "name is a type".</returns>
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
+
+/// <summary>
+/// Interface for interactive objects.
+/// </summary>
+public interface IInteractive
+{
+    /// <summary>
+    /// Method to interact with the object.
+    /// </summary>
+    void Interact();
+}
+
+/// <summary>
+/// Interface for breakable objects.
+/// </summary>
+public interface IBreakable
+{
+    /// <summary>
+    /// Gets or sets the durability of the object.
+    /// </summary>
+    int durability { get; set; }
+
+    /// <summary>
+    /// Method to break the object.
+    /// </summary>
+    void Break();
+}
+
+/// <summary>
+/// Interface for collectable objects.
+/// </summary>
+public interface ICollectable
+{
+    /// <summary>
+    /// Gets or sets whether the object is collected.
+    /// </summary>
+    bool isCollected { get; set; }
+
+    /// <summary>
+    /// Method to collect the object.
+    /// </summary>
+    void Collect();
+}
+
+/// <summary>
+/// TestObject class inheriting from Base and implementing IInteractive, IBreakable, and ICollectable.
+/// </summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    /// <summary>
+    /// Gets or sets the durability of the object.
+    /// </summary>
+    public int durability { get; set; }
+
+    /// <summary>
+    /// Gets or sets whether the object is collected.
+    /// </summary>
+    public bool isCollected { get; set; }
+
+    /// <summary>
+    /// Method to interact with the object.
+    /// </summary>
+    public void Interact()
+    {
+        // Minimal implementation
+    }
+
+    /// <summary>
+    /// Method to break the object.
+    /// </summary>
+    public void Break()
+    {
+        // Minimal implementation
+    }
+
+    /// <summary>
+    /// Method to collect the object.
+    /// </summary>
+    public void Collect()
+    {
+        // Minimal implementation
+    }
+}
