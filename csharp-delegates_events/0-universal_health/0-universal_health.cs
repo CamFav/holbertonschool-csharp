243ebd88f6f13d440fffa9c7b58b2024734f834e
CamilleFavriel <camillefvrl@gmail.com>
2024-12-17 15:59:20 +0100

    Task 0: Player Public Class & PrintHealth() method

---
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+/// <summary>
+/// 
+/// </summary>
+public class Player
+{
+    private string _name;
+    private float _maxHp;
+    private float _hp;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Name
+    {
+        get { return _name; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public float MaxHp
+    {
+        get { return _maxHp; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public float Hp
+    {
+        get { return _hp; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="maxHp"></param>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        _name = name;
+
+        if (maxHp > 0)
+        {
+            _maxHp = maxHp;
+        }
+        else
+        {
+            _maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+
+        _hp = _maxHp;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{_name} has {_hp} / {_maxHp} health");
+    }
+}
